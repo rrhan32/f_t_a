ad27fc786696bbf7e7070db05dfbd939a75c3fc2
rrhan32 <raj.32@iitj.ac.in>
2025-06-01 00:41:29 +0530

    loading feature implemented.

--- frontend/src/submit.js
+++ frontend/src/submit.js
@@ -2,6 +2,9 @@
 import { useStore } from './store'; // Assuming you’re using Zustand or similar
 import { shallow } from 'zustand/shallow';
 import Button from '@mui/material/Button';
+import Backdrop from '@mui/material/Backdrop';
+import CircularProgress from '@mui/material/CircularProgress';
+import { useState } from 'react';
 
 const selector = (state) => ({
   nodes: state.nodes,
@@ -10,8 +13,10 @@ const selector = (state) => ({
 
 export const SubmitButton = () => {
   const { nodes, edges } = useStore(selector, shallow);
+  const [loading, setLoading] = useState(false);
 
   const handleSubmit = async () => {
+    setLoading(true);
       console.log('Submitting pipeline with nodes:', nodes, 'and edges:', edges);
     try {
       const response = await fetch('http://localhost:8000/pipelines/parse', {
@@ -35,12 +40,23 @@ export const SubmitButton = () => {
     } catch (error) {
       console.error('Error submitting pipeline:', error);
       alert('❌ Error submitting pipeline. Check the console for details.');
+    } finally {
+     setLoading(false);
     }
   };
 
     return (
         <div style={{display: 'flex', alignItems: 'center', justifyContent: 'center'}}>
             <Button variant="contained" color='success' type="submit" onClick={handleSubmit}>Submit</Button>
+            {loading && (
+              <Backdrop
+                sx={(theme) => ({ color: '#fff', zIndex: theme.zIndex.drawer + 1 })}
+                open={loading}
+                // onClick={handleClose}
+              >
+                <CircularProgress color="inherit" />
+              </Backdrop>
+            )}
         </div>
     );
 }
